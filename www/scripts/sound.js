883e8c9b14d93c9492dc874debc5eb5823e40f24
inferno@hmstr <inferrna@gmail.com>
2014-08-31 18:36:00 +0900

    +

--- www/scripts/sound.js
+++ www/scripts/sound.js
@@ -1,12 +1,15 @@
 define(
     ['sharedc', 'stuff', 'swac'],
   function(sharedc, stuff, swac){
-        var swpths = [];
+        var count = 0;;
         var idx = 0;
         var word = '';
         var audios = [];
         var intrvl = null;
         var snd = document.createElement("audio");
+        var sndcnt = document.getElementById('sndcnt');
+        var sndbt = document.getElementById('sndbt');
+        var nosnd = document.getElementById('nosnd');
         snd.preload = "metadata";
         function utf8_to_b64( str ) {
             return window.btoa(encodeURIComponent( escape( str )));
@@ -27,7 +30,9 @@ define(
             }
         }
         function play(){
-            if(swpths[idx]){
+            console.log("Clicked sndbt");//NFP
+            console.log("count == "+count);//NFP
+            if(count){
                 audios[idx].load();
                 clearInterval(intrvl);
                 intrvl = setInterval(
@@ -35,9 +40,10 @@ define(
                         if (audios[idx].readyState>0){
                             clearInterval(intrvl);
                             audios[idx].play();
+                            console.log("Sound played");//NFP
                             nidx = idx+1;
-                            idx = nidx < swpths.length ? nidx : 0;
-                            sndcnt.textContent = (idx+1)+"/"+swpths.length;
+                            idx = nidx < count ? nidx : 0;
+                            sndcnt.textContent = (idx+1)+"/"+count;
                         } else {
                             console.log(audios[idx]);//NFP
                         }
@@ -46,26 +52,20 @@ define(
             }
         }
 
-        /*snd.addEventListener("canplay", canplay);
-        snd.addEventListener("loadeddata", canplay);
-        snd.addEventListener("loadstart", canplay);//Too early
-        snd.addEventListener("canplaythrough", canplay);*/
         audios.push(snd);
-        var sndcnt = document.getElementById('sndcnt');
-        var sndbt = document.getElementById('sndbt');
-        var nosnd = document.getElementById('nosnd');
-        sndbt.onclick=function(){play();};
+        sndbt.onclick = play;
         sndbt.style.height = Math.round(32*(window.devicePixelRatio || 1.0))+"px";
         sndbt.style.width = sndbt.style.height;
         sndbt.style.backgroundImage = 'url('+stuff.sndimg+')';
         sharedc.register('dict', 'change_lng', function(lang){
-                   if(lang.length===2) var lidx = swac.lparts[lang];
-                   else var lidx = lang;
-                   swac.init(lidx, got_swac);
-        });
+                       if(lang.length===2) var lidx = swac.lparts[lang];
+                       else var lidx = lang;
+                       swac.init(lidx, got_swac);
+            });
         function got_swac(swpths){
                if(swpths && swpths.length>0){
                    idx = 0;
+                   count  = swpths.length;
                    if(swpths.length > audios.length) add_audios(swpths.length);
                    for(var i=0; i<swpths.length; i++){
                        var pid = swpths[i][0];
@@ -75,9 +75,9 @@ define(
                    console.log("swpths:");//NFP
                    console.log(swpths);//NFP
                    sndbt.style.display = 'inline'; nosnd.style.display = 'none'; 
-                   sndcnt.textContent = 1+"/"+swpths.length;
+                   sndcnt.textContent = 1+"/"+count;
                    sndbt.style.display = 'block';
-               } else {sndbt.style.display = 'none'; nosnd.style.display = 'inline';}
+               } else {count = 0; sndbt.style.display = 'none'; nosnd.style.display = 'inline';}
         }
 
         return {
@@ -95,7 +95,7 @@ define(
                    swac.get(idw);
                } else {
                    nidx = idx+1;
-                   idx = nidx < swpths.length ? nidx : 0;
+                   idx = nidx < count ? nidx : 0;
                }
             }
         }
