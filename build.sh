ba8be9194c45f437eea5932f2e6726170a8d4916
inferno <inferrna@gmail.com>
2024-01-08 15:15:56 +0400

    Push to /sdcard/

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ printf "Remove old android files\n"
 npx cordova platform rm android
 printf "\nAdd new android\n"
 npx cordova platform add android@4.0.2  --verbose
-printf "\nSet minSdkVersion to 9\n"
-sed -i -e 's:minSdkVersion="10":minSdkVersion="9":g' ./platforms/android/CordovaLib/AndroidManifest.xml
+printf "\nSet minSdkVersion to 8\n"
+sed -i -e 's:minSdkVersion="10":minSdkVersion="8":g' ./platforms/android/CordovaLib/AndroidManifest.xml
 printf "\nBuild android\n"
 ANDROID_HOME=~/.dev/Alien/AndroidOld JAVA_HOME=/home/inferno/.jdks/corretto-1.8.0_392 npx cordova build android --verbose
 #ANDROID_HOME=~/.dev/Alien/AndroidOld JAVA_HOME=/home/inferno/.jdks/java-6-openjdk-amd64 npx cordova build android --verbose
@@ -14,3 +14,5 @@ printf "\nClear logs\n"
 adb logcat -c
 printf "\nPush book\n"
 adb push prolog.epub /data/data/com.example.abread/lib/prolog.epub
+printf "\nPush book to /sdcard/\n"
+adb push prolog.epub /sdcard/
